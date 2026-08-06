1b375ac5f4cab5113f757d444b9fe362a72dfb8e
Akhilbellana <akhilbellana22@gmail.com>
2026-08-06 14:44:06 +0530

    Implement solution to find duplicate and missing using map

---
+++ 645-set-mismatch/set-mistach-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Map<Integer,Integer>map=new HashMap<>();
+        List<Integer>list=new ArrayList<>();
+        int duplicate=-1;
+        int missing=-1;
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(int i=1;i<=nums.length;i++){
+            if(map.containsKey(i)){
+                if(map.get(i)>1){
+                duplicate=i;
+            }
+            }else{
+            missing=i;
+            }
+        }
+        list.add(duplicate);
+        list.add(missing);
+        int[] a=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            a[i]=list.get(i);
+        }
+        return a;
+    }
+}
