02027d93567d02e0cc023c5e1caed020d7d44ee4
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-26 23:50:22 +0530

    Time: 39 ms (24.59%) | Memory: 57.5 MB (5.01%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int n:people){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        int c=0;
+        int i=0;
+        int j=people.length-1;
+        Arrays.sort(people);
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                c++;
+                j--;
+                i++;
+            }else{
+                j--;
+                c++;
+            }
+        }
+        return c;
+        
+    }
+}
