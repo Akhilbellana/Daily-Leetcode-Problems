7dfb0078d9ee40394523a1ec2a2f1b3cfe20ce4e
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-03 22:49:30 +0530

    Update maximum-product-of-two-elements-in-an-array.java

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -3,6 +3,6 @@ class Solution {
         Arrays.sort(nums);
         int i=nums.length-2;
         int j=nums.length-1;
-        return ((nums[i]-1)*(nums[j]-1));
+    return ((nums[i]-1)*(nums[j]-1));
     }
 }
